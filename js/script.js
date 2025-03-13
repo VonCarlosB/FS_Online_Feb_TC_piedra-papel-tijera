283424d52aa8d9136d23b721e4a5bbde266f6e7b
CarlosBF <cborrellf@outlook.es>
2025-03-13 11:29:20 +0100

    Finnished the style and added a feedback feature and end of game pop-up

--- js/script.js
+++ js/script.js
@@ -22,13 +22,40 @@ function jugada(election) {
 function checkResults(ans, election) {
     if(election === ans){
         result.innerText += 'Empate a '+ans
+        result.style.backgroundColor = 'beige'
     }else if(election === 'tijera' && ans ==='piedra' || election==='piedra' && ans ==='papel' || election==='papel' && ans ==='tijera'){
         result.innerText += 'Has perdido con '+election+' contra '+ans
         computerPoints++
         computerCounter.innerText = 'Puntos de la máquina: '+computerPoints
+        result.style.backgroundColor = 'lightcoral'
     }else{
         result.innerText += 'Has ganado con '+election+' contra '+ans
         playerPoints++
         userCounter.innerText = 'Tus puntos: '+playerPoints
+        result.style.backgroundColor = 'lightgreen'
+    }
+    changeScale()
+}
+
+function changeScale() {
+    let dif = playerPoints - computerPoints
+    console.log(dif)
+    userCounter.style.width = (50+dif*5)+'%'
+    computerCounter.style.width = (50-dif*5)+'%'
+    setTimeout(() => {
+        reloadPage(dif)
+    }, 500);
+}
+
+function reloadPage(dif) {
+    if(dif >= 10){
+        if(window.confirm('Enhorabuena, has ganado, tienes mucha suerte.\n¿Otra vez?')){
+            location.reload()
+        }
+    }
+    if(dif <= -10){
+        if(window.confirm('Vaya... Has perdido, más suerte la próxima vez.\n¿Otra vez?')){
+            location.reload()
+        }
     }
 }
